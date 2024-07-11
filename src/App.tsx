2cfe628c7c1c59e4d62a8cd7d997e4f32fc24f92
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-07-11 13:28:59 +0530

    Added Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import Aboutme from "./components/Aboutme";
 import Cards from "./components/Cards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <div className="min-h-screen ml-[16px] mr-[16px] lg:ml-[200px] lg:mr-[200px] font-madeTommy">
+        <Analytics />
         <Navbar />
         <Hero />
         <Cards />
